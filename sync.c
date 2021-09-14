122194c36637e6825bdea53383c8bca0a191a51a
yakoub abaya <sting606@gmail.com>
2021-09-14 10:06:39 +0300

    back to game.log

--- sync.c
+++ sync.c
@@ -8,7 +8,9 @@
 
 typedef struct {
   int wd;
+  FILE* game_log;
   FILE* console_log;
+  long game_pos;
   long console_pos;
   const char *path;
   GameScore *game;
@@ -58,17 +60,19 @@ int sync_logs_find(int wd) {
 }
 
 void sync_logs_assign(int wd, const char* path) {
-  char filename[64];
+  char filename[128];
   for (int i=0; i<SLOTS; i++) {
     if (game_slots[i].wd == -1) {
       game_slots[i].wd = wd;
-      strncpy(filename, path, 50);
+      strncpy(filename, path, 100);
+      game_slots[i].game_log = fopen(strcat(filename, "/game.log"), "r");
+      strncpy(filename, path, 100);
       game_slots[i].console_log = fopen(strcat(filename, "/etconsole.log"), "r");
+      game_slots[i].game_pos = 0;
       game_slots[i].console_pos = 0;
       game_slots[i].path = path;
       game_slots[i].game = malloc(sizeof(GameScore));
       tce_parse_game_init(game_slots[i].game);
-
       debug_info(DBGLVL, "wd %d assinged to %d\n", wd, i);
       return;
     }
@@ -79,6 +83,7 @@ void sync_logs_assign(int wd, const char* path) {
 void sync_logs_close() {
   for (int i=0; i<SLOTS; i++) {
     if (game_slots[i].wd != -1) {
+      fclose(game_slots[i].game_log);
       fclose(game_slots[i].console_log);
     }
   }
@@ -87,16 +92,24 @@ void sync_logs_close() {
 void sync_logs_rewind(const char* name, int wd) {
   int at = sync_logs_find(wd);
   game_dir *slot = &game_slots[at];
-  char filename[32];
+  char filename[128];
+  strncpy(filename, slot->path, 100);
 
-  if (strcmp(name, "etconsole.log") == 0) {
+  if (strcmp(name, "game.log") == 0) {
+    if (slot->game_log) {
+      fclose(slot->game_log);
+    }
+    slot->game_log = fopen(strcat(filename, "/game.log"), "r");
+    slot->game_pos = 0;
+    debug_info(DBGLVL , "game.log rewind for wd %s", wd);
+  }
+  if (strcmp(name, "console.log") == 0) {
     if (slot->console_log) {
       fclose(slot->console_log);
     }
-    strcpy(filename, slot->path);
     slot->console_log = fopen(strcat(filename, "/etconsole.log"), "r");
     slot->console_pos = 0;
-    debug_info(DBGLVL , "etconsole.log rewind for wd %s", wd);
+    debug_info(DBGLVL , "console.log rewind for wd %s", wd);
   }
 }
 
@@ -105,16 +118,24 @@ void sync_logs(const char *name, int wd) {
   static char buff[BUFF_SIZE];
   int seek_ret;
   long int *pos;
-  FILE *logfile = NULL;
+  FILE *logfile;
+  void (*parse)(const char*, GameScore*);
 
-
-  if (strcmp(name, "etconsole.log") == 0) {
-    logfile = game_slots[at].console_log;
-    pos = &game_slots[at].console_pos;
-    debug_info(DBGLVL+1, "etconsole.log for ws=%d\n", wd);
+  if (strcmp(name, "game.log") == 0) {
+    logfile = game_slots[at].game_log;
+    pos = &game_slots[at].game_pos;
+    parse = tce_parse;
   }
   else {
-    return;
+    if (strcmp(name, "etconsole.log") == 0) {
+      logfile = game_slots[at].console_log;
+      pos = &game_slots[at].console_pos;
+      parse = tce_parse_guid;
+      debug_info(DBGLVL+1, "etconsole.log for ws=%d", wd);
+    }
+    else {
+      return;
+    }
   }
 
   if (!logfile) {
