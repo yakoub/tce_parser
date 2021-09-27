dc68b759c678a24d85925c136c87fc9ea3eaa95c
yakoub abaya <sting606@gmail.com>
2021-09-27 14:55:45 +0300

    read default reset and make clean

--- Makefile
+++ Makefile
@@ -2,10 +2,14 @@ myinc != mariadb_config --include
 mylib != mariadb_config --libs
 debug = -g 
 objects = bin/watch.o bin/sync.o bin/tce_parse.o bin/data.o bin/config.o
+
+all: tce_watch tce_reset
+
 tce_watch: ${objects}
 	gcc ${debug} ${objects} ${mylib} -o tce_watch 
-tce_reset: sync_reset.c
-	gcc sync_reset.c -o tce_reset
+tce_reset: sync_reset.c bin/config.o
+	gcc bin/config.o sync_reset.c -o tce_reset
+
 bin/watch.o: watch.c sync.h data.h debug.h
 	gcc ${debug} -c watch.c -o bin/watch.o
 bin/sync.o: sync.c sync.h tce_parse.h debug.h
@@ -17,4 +21,4 @@ bin/data.o: data.c data.h debug.h
 bin/config.o: data.h conf.c
 	gcc ${debug} -c conf.c -o bin/config.o
 clean:
-	rm bin/* tce_watch
+	rm bin/* tce_watch tce_reset
--- sync_reset.c
+++ sync_reset.c
@@ -6,27 +6,36 @@ typedef struct {
   long console_pos;
 } dir_watch;
 
+char ** config_paths(int *);
+
 void main(int argc, char **argv) {
   FILE *pos_track;
   char filename[128];
   dir_watch dwatch;
 
-  if (argc < 2) {
-    printf("no given paths\n");
-    return;
-  }
+  int path_count;
+  char **paths = config_paths(&path_count);
 
-  strncpy(filename, argv[1], 100);
-  strcat(filename, "/.tce_watch");
-  pos_track = fopen(filename, "r+b");
-  if (!pos_track) {
-    printf("failed to open file\n");
-    return;
+  if (argc == 2) {
+    path_count = 1;
+    paths = argv + 1;
   }
-  fread(&dwatch, sizeof(dir_watch), 1, pos_track);
-  printf("game = %ld console = %ld\n", dwatch.game_pos, dwatch.console_pos);
-  dwatch.console_pos = 0;
-  rewind(pos_track);
-  fwrite(&dwatch, sizeof(dir_watch), 1, pos_track);
-  printf("reset console pos\n");
+  else {
+    char **paths = config_paths(&path_count);
+  }
+  
+  for (int i=0; i<path_count; i++) {
+    strncpy(filename, paths[i], 100);
+    strcat(filename, "/.tce_watch");
+    pos_track = fopen(filename, "r+b");
+    if (!pos_track) {
+      printf("failed to open file %s\n", filename);
+      continue;
+    }
+    fread(&dwatch, sizeof(dir_watch), 1, pos_track);
+    printf("game = %ld console = %ld\n", dwatch.game_pos, dwatch.console_pos);
+    dwatch.console_pos = 0;
+    rewind(pos_track);
+    fwrite(&dwatch, sizeof(dir_watch), 1, pos_track);
+  }
 }
