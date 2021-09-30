0be3482c97ba9a164447320d715b332d41fa3e04
yakoub abaya <sting606@gmail.com>
2021-09-30 23:44:54 +0300

    config

--- sync_reset.c
+++ sync_reset.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include "conf.h"
 
 typedef struct {
   long game_pos;
   long console_pos;
 } dir_watch;
-
-char ** config_paths(int *);
 
 void main(int argc, char **argv) {
   FILE *pos_track;
@@ -14,18 +13,15 @@ void main(int argc, char **argv) {
   dir_watch dwatch;
 
   int path_count;
-  char **paths = config_paths(&path_count);
+  PathConfig *conf = config_paths(&path_count);
 
   if (argc == 2) {
     path_count = 1;
-    paths = argv + 1;
-  }
-  else {
-    char **paths = config_paths(&path_count);
+    conf[0].path = argv[1];
   }
   
   for (int i=0; i<path_count; i++) {
-    strncpy(filename, paths[i], 100);
+    strncpy(filename, conf[i].path, 100);
     strcat(filename, "/.tce_watch");
     pos_track = fopen(filename, "r+b");
     if (!pos_track) {
