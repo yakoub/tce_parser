d4a482bb308d426850e5d740b12deb6a794d5e36
yakoub abaya <sting606@gmail.com>
2021-09-30 22:37:35 +0300

    rename

--- watch.c
+++ watch.c
@@ -14,19 +14,19 @@ void watch_handler(int ino_desc);
 int main(int argc, char* argv[]) {
   int ino_desc = inotify_init();  
   int path_count;
-  PatchConfig paths = config_paths(&path_count);
+  PatchConfig conf = config_paths(&path_count);
   int *ino_watch = malloc(path_count * sizeof(int));
 
   sync_logs_init();
 
   for (int i=0; i < path_count; i++) {
-    ino_watch[i] = inotify_add_watch(ino_desc, paths[i].path, IN_MODIFY | IN_CREATE);
+    ino_watch[i] = inotify_add_watch(ino_desc, conf[i].path, IN_MODIFY | IN_CREATE);
     if (ino_watch[i] == -1) {
-      printf("watch error %d, path %s", errno, paths[i]);
+      printf("watch error %d, path %s", errno, conf[i].path);
       return 1;
     }
     else {
-      sync_logs_assign(ino_watch[i], paths[i]);
+      sync_logs_assign(ino_watch[i], conf[i]);
     }
   }
 
