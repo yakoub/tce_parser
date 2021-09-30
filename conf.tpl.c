dc2a8f7029f7f1ab53d09f11340f112d67775735
yakoub abaya <sting606@gmail.com>
2021-09-30 22:33:20 +0300

    example configuration

--- conf.tpl.c
+++ conf.tpl.c
@@ -1,15 +1,27 @@
 #include "data.h"
+#include "conf.h"
 #include <stddef.h>
 
 #define PATHS 2
-char *paths[PATHS] = {
-  "./watch.d",
-  "./watch2.d"
+
+PathConfig path_conf[PATHS] = {
+  {
+    .path = "./watch.d", 
+    .hostname = "^3first^rHost", 
+    .ip = "127.0.0.1", 
+    .port = 27960
+  },
+  {
+    .path = "./watch2.d", 
+    .hostname = "^?second^8Host", 
+    .ip = "127.0.0.2", 
+    .port = 27920
+  }
 };
 
-char** config_paths(int *count) {
+PathConfig* config_paths(int *count) {
   *count = PATHS;
-  return paths;
+  return path_conf;
 }
 
 void config_data(DataConfig *my) {
